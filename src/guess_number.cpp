a1a87bc9403dfd035e4766f668dbb1866bc56e03
achraf lemghari <hraf.lemghari@gmail.com>
2022-03-04 10:16:24 +0100

    Add input controle for Menu and player guess

--- src/guess_number.cpp
+++ src/guess_number.cpp
@@ -19,7 +19,7 @@ int get_choice( )
     //Error check
     while (!std::cin.good())
     {
-        std::cout << " Invalid choice ! please try again .";
+        std::cout << " Invalid input! please try again ."<< std::endl;
 
         std::cin.clear();
         std::cin.ignore(INT_MAX,'\n');
@@ -33,27 +33,41 @@ int get_choice( )
 
     return choice;
 }
+void option_control(int min,int max,int player_choice)
+{
+    if (player_choice < min || player_choice > max) {
+            std::cout << "Out of the range of the game ! Remamber the correct number is between " << min<<" and "<<max<< std::endl;
+        }
+}
+
 int game_level()
 {
-    std::cout << "Choose from 1 to 3 how lucky you think you are today : \n 1-Lucky \t 2-Very lucky \t 3-God of luck "<< std::endl ;
+    bool valid_choice = false;
     int choice,max_to_guess=100;
+    while (!valid_choice) {
+    std::cout << "Choose from 1 to 3 how lucky you think you are today : \n 1-Lucky \t 2-Very lucky \t 3-God of luck "<< std::endl ;
     choice=get_choice();
     
-   switch(choice) {
-  case 1:
-    max_to_guess=10;
-    break;
-  case 2:
-    max_to_guess=50;
-    break;
-  case 3:
-    max_to_guess=100;
-    break;
-  default:
-		std::cout << "invalid choice !"<< std::endl ;
-		game_level() ;
-    break;
-}
+    
+        switch(choice) {
+        case 1:
+            max_to_guess=10;
+            valid_choice=true;
+            break;
+        case 2:
+            max_to_guess=50;
+            valid_choice=true;
+            break;
+        case 3:
+            max_to_guess=100;
+            valid_choice=true;
+            break;
+        default:
+                std::cout << "invalid choice ! try again "<< std::endl ;
+                valid_choice=false;
+            break;
+        }
+    }
  return max_to_guess;
    
 }
@@ -71,14 +85,15 @@ void guess_number()
     bool finished = false;
     while (!finished) {
         int player_guess=get_choice();
-        if (player_guess< number_to_guess) {
+        option_control(min,max,player_guess);
+        if (player_guess< number_to_guess && player_guess > min ) {
             std::cout << "wrong ! Guess a number greater then " << player_guess << std::endl;
         }
-        else if (player_guess > number_to_guess) {
+        else if (player_guess > number_to_guess && player_guess < max ) {
             std::cout << "wrong ! Guess a number lower then " << player_guess << std::endl;}
-        else {
+        else if (player_guess < max && player_guess < max ){
             std::cout << "Congrats, you won!\n";
             finished = true;
             }
     }
-    }
+}
--- src/guess_number.h
+++ src/guess_number.h
@@ -3,3 +3,4 @@ void guess_number();
 int game_level();
 int get_choice();
 int rand(int min, int max);
+void option_control(int min,int max,int user_choice);
