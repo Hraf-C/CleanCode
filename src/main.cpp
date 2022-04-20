034f32eb6c5450192a5434f05817bd94ff4acdad
achraf lemghari <hraf.lemghari@gmail.com>
2022-04-20 17:02:23 +0200

    add p6

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include "guess_number.h"
 #include "hangman.h"
 #include "menu.h"
-
-
-
+#include <p6/p6.h>
+#include "menu.h"
 
 int main()
+{
+    auto ctx = p6::Context{{1280, 720, "playground"}}; 
+    ctx.start();                                       
+}
+
+
+
+/*int main()
 {
    Menu();
     
     return 0;
-}
+}*/
